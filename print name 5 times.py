da6633aee3ff3aa4220eb0138afa6cbe2fc28cda
jeevankumarprogram <135030928+jeevankumarprogram@users.noreply.github.com>
2023-06-21 17:56:09 +0530

    Add files via upload

---
+++ print name 5 times.py
@@ -0,0 +1,6 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 18 2019, 23:46:00) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name="jeevankumar"
+>>> name*5
+'jeevankumarjeevankumarjeevankumarjeevankumarjeevankumar'
+>>> 
